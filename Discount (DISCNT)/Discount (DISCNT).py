8472fd9c857e821636c2ee9f8ee2fa6d04945ade
Anand Kumar <111251492+kumarAnand05@users.noreply.github.com>
2022-09-13 23:51:50 +0530

    Add files via upload

---
+++ Discount (DISCNT)/Discount (DISCNT).py
@@ -0,0 +1,9 @@
+test_cases = int(input())
+for n in range(test_cases):
+    input_percentage = int(input())
+    print((100-input_percentage))
+
+"""
+The above code works by taking the input percentage and subtracting it from 100.
+The output is the remaining percentage.
+"""
